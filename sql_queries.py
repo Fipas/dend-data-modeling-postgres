2b784af2b7cdfd6f70e5a81b0ce9bdc6a466c4f2
root <fonsecafel@gmail.com>
2020-05-02 15:41:41 +0000

    Updated README and changed sql query to update user level on conflict

--- sql_queries.py
+++ sql_queries.py
@@ -10,7 +10,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays(
-        sonplay_id SERIAL PRIMARY KEY,
+        sonplay_id SERIAL PRIMARY KEY NOT NULL,
         start_time timestamp,
         user_id int,
         level text,
@@ -24,7 +24,7 @@ songplay_table_create = ("""
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users(
-        user_id int PRIMARY KEY,
+        user_id int PRIMARY KEY NOT NULL,
         first_name text,
         last_name text,
         gender char,
@@ -34,7 +34,7 @@ user_table_create = ("""
 
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS songs(
-        song_id text PRIMARY KEY,
+        song_id text PRIMARY KEY NOT NULL,
         title text,
         artist_id text,
         year int,
@@ -44,7 +44,7 @@ song_table_create = ("""
 
 artist_table_create = ("""
      CREATE TABLE IF NOT EXISTS artists(
-        artist_id text PRIMARY KEY,
+        artist_id text PRIMARY KEY NOT NULL,
         name text,
         location text,
         latitude float,
@@ -54,7 +54,7 @@ artist_table_create = ("""
 
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time(
-        start_time timestamp PRIMARY KEY,
+        start_time timestamp PRIMARY KEY NOT NULL,
         hour int,
         day int,
         week int,
@@ -87,7 +87,8 @@ user_table_insert = ("""
         gender,
         level
     ) VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING
+    ON CONFLICT (user_id) DO UPDATE SET 
+        level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
